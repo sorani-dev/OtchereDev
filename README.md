9e634ba92c27456ccef614fcfd8c4d5715fc76e8
simon441 <simon.cateau@yahoo.fr>
2022-01-13 22:33:05 +0100

    Initial commit - from starter code

---
+++ README.md
@@ -0,0 +1,1 @@
+# Django / Tailwind Tutorial - Code a Netflix Clone
---
+++ django_netflix_clone/django_netflix/urls.py
@@ -0,0 +1,18 @@
+
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
