55e7c10bccfe527f938d813b08cb29ca0e70c142
simon441 <simon.cateau@yahoo.fr>
2022-01-15 18:29:34 +0100

    feat: add profile list

--- django_netflix_clone/core/urls.py
+++ django_netflix_clone/core/urls.py
@@ -1,10 +1,11 @@
 import imp
 from django.urls import path
 
-from .views import Index
+from .views import Index, ProfileList
 
 app_name = 'core'
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('profile/', ProfileList.as_view(), name='profile_list'),
 ]
--- django_netflix_clone/core/views.py
+++ django_netflix_clone/core/views.py
@@ -1,11 +1,28 @@
 from typing import Dict, List
+
+from django.contrib.auth.decorators import login_required
+from django.db.models import QuerySet
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
 from django.views import View
+
+from .models import Profile
 
 # Create your views here.
 
 
 class Index(View):
     def get(self, request: HttpRequest, *args: List, **kwargs: Dict) -> HttpResponse:
+        if request.user.is_authenticated:
+            return redirect(to='core:profile_list')
         return render(request=request, template_name='index.html')
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileList(View):
+    def get(self, request: HttpRequest, *args: List, **kwargs: Dict) -> HttpResponse:
+        profiles: QuerySet[Profile] = request.user.profiles.all()
+        return render(request, 'profileList.html', {
+            'profiles': profiles
+        })
