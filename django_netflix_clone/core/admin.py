7d44fa7b4cb0a86b212f4669706f419b9b7fc532
simon441 <simon.cateau@yahoo.fr>
2022-01-15 20:14:20 +0100

    feat: add models to admin backend

--- django_netflix_clone/core/admin.py
+++ django_netflix_clone/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import CustomUser, Movie, Profile, Video
+
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Profile)
+admin.site.register(Video)
+admin.site.register(CustomUser)
